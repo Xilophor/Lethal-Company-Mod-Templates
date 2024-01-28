74c37734e96c196d37e28f007b1b263ebd2aab58
Xilophor <ryanzgreg@gmail.com>
2024-01-28 14:47:23 -0500

    Add NetcodePatcher Support

---
+++ templates/install-netcode-patcher.cmd
@@ -0,0 +1,4 @@
+cd ModTemplate
+dotnet new tool-manifest
+dotnet tool install --local evaisa.netcodepatcher.cli
+del ../install-netcode-patcher.cmd
---
+++ templates/install-netcode-patcher.sh
@@ -0,0 +1,4 @@
+cd ModTemplate
+dotnet new tool-manifest
+dotnet tool install --local evaisa.netcodepatcher.cli
+rm ../install-netcode-patcher.sh
